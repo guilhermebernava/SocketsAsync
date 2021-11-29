c4cf119ddde0ecb4421ac62f98c51cf58a6e1ed5
bernava <guilhermebernava00@gmail.com>
2021-11-29 10:51:46 -0300

    Working Properly

---
+++ AsyncSocktes/AsyncSocktes/Server.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+
+namespace AsyncSocktes
+{
+    internal class Server
+    {
+        public Socket Svr { get; set; }
+        public bool online = true;
+
+        public async void Connect()
+        {
+            
+
+        }
+
+    }
+}
